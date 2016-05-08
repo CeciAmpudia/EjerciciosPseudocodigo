a98c9157ca062cbd83724d3c56f3e5e175214dfd
CeciAmpudia <ceciliag.ampudia@gmail.com>
2016-05-08 14:00:39 -0500

    Pseudocodigo

---
+++ main.js
@@ -0,0 +1,69 @@
+// EJERCICIO #2
+/* var a = prompt("Dame el primer numero");
+var b = prompt("Dame el segundo numero");
+var c = prompt("Dame el tercer numero");
+var promedio = (parseInt(a) + parseInt(b) + parseInt(c))/3;
+
+alert("Promedio: " + promedio); */
+
+
+// EJERCICIO #3
+/* var b =	prompt("Cual es el tamano de la base?");
+var a = prompt("Cual es el tamano de la altura?");
+var area = (parseInt(b) * parseInt(a))/2;
+
+alert("El area del triangulo es: " + area); */
+
+
+// EJERCICIO #4
+/* var litros = prompt("Cuantos litros se produce en un dia?")
+var galones = parseInt(litros)/3.785;
+
+alert("La cantidad de galones es: " + galones); */
+
+
+// EJERCICIO #5
+/*var horas = prompt("Horas trabajadas al dia");
+var pago = prompt("Cual es el pago por hora?");
+var sueldo = (parseInt(horas) * parseInt(pago) * 7);
+
+alert("El sueldo semanal es: " + sueldo); */
+
+
+// EJERCICIO 6
+/* var metros = prompt("Cuantos metros de tela se tiene?");
+var pulgadas = parseInt(metros)/0.0254;
+
+alert("Pulgadas: " + pulgadas); */
+
+
+// EJERCICIO #7
+/*var precio = prompt("Cual es el precio por m2?");
+var metro = prompt("Cuantos m2 son?");
+var presupuesto = parseInt(metro) * parseInt(precio);
+
+alert("El presupuesto es: " + presupuesto); */
+
+
+// EJERCICIO 8
+/* var km = prompt("Cuantos kilometros recorrera?");
+var precio = prompt("Cual es el costo por promedio?");
+var costo = parseInt(km) * parseInt(precio);
+
+alert("El costo del boleto es: " + costo); */
+
+
+// EJERCICIO 9
+/* var costo = prompt("Costo por minuto");
+var tiempo = prompt("Duracion de la llamada en minutos");
+var total = parseInt(costo) * parseInt(tiempo);
+
+alert("El costo de la llamada es: " + total); */
+
+
+// EJERCICIO 10
+/* var precio = prompt("Precio de habitacion");
+var noches = prompt("Cuantas noches de hospedaje?");
+var total = parseInt(precio) * parseInt(noches);
+
+alert("Total: " + total); */
